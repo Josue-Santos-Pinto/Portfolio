c641460dc76622579649c33a704dfc48bb81a9c4
Josue-Santos-Pinto <js.pinto55@gmail.com>
2023-04-10 20:17:27 -0300

    changing package files

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Welcome } from './src/screens/Welcome';
-import { NavigationContainer } from '@react-navigation/native';
+import {Welcome} from './src/screens/Welcome';
+import {NavigationContainer} from '@react-navigation/native';
 import MainStack from './src/routes/MainStack';
 
 export default () => {
